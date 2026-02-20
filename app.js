a048451c0b2f541ba35b40a7b74cc7c7eff914b4
Thanuja_313 <sewmini234@gmail.com>
2026-02-20 14:01:56 +0530

    Implement async and await for handling Promise

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const myPromise=new Promise((resolve,reject)=>{
  }
 });
 
-myPromise.then((result)=>{
- console.log(result);
-}).catch((error)=>{
- console.log(error);
-});
+async function myFunction(){
+ try{
+  const result=await myPromise;
+  console.log(result);
+ }catch(error){
+  console.log(error);
+ }
+}
+
+myFunction();
